2e75641d4a304de5938580dc5c105136db2f1e62
prabhpreet-singh <prabhpreetsingh.singh0203@gmail.com>
2022-08-26 18:40:40 +0530

    Update welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -1,1 +1,10 @@
+$(window).on("load",function(){
+  $(".loader-wrapper").delay(1000).fadeOut("slow");
+});
+
+var username = JSON.parse(localStorage.getItem('usname'));
+
+document.getElementById("uname").innerHTML = `${username}!`;
+
+
 
